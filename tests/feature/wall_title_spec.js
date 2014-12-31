f7fd31768d3740039404449854bdf06a7dcd30cd
Pete Hodgson <git@thepete.net>
2014-12-30 16:33:30 -0800

    cleanup cruft

--- tests/feature/wall_title_spec.js
+++ tests/feature/wall_title_spec.js
@@ -1,13 +1,11 @@
 describe( 'the wall title', function(){
   specify('a wall starts off with a sensible default title', function(){
-    var foo = window;
-    console.dir(foo);
-    var bar = window.testHarness;
-    var baz = window.testHarness.bootIsolatedApp;
     $appContainer = $(window.testHarness.bootIsolatedApp());
 
     $wallTitle = $appContainer.find(".title");
     expect($wallTitle).to.exist;
     expect($wallTitle).to.have.text('The Wall');
   });
+
+
 });
